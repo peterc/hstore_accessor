d6c5eb89cabb3f3c18496105d09a13f8124f8384
Peter Cooper <git@peterc.org>
2014-08-28 00:59:05 +0100

    Support Rails 4.2.0 beta

--- lib/hstore_accessor/type_helpers.rb
+++ lib/hstore_accessor/type_helpers.rb
@@ -3,21 +3,11 @@ module HstoreAccessor
     def self.cast(type, value)
       return nil if value.nil?
 
-      column_class = ActiveRecord::ConnectionAdapters::Column
-
       case type
       when :string, :hash, :array, :decimal
         value
-      when :integer
-        column_class.value_to_integer(value)
-      when :float
-        value.to_f
-      when :time
-        TimeHelper.string_to_time(value)
-      when :date
-        column_class.value_to_date(value)
-      when :boolean
-        column_class.value_to_boolean(value)
+      when :integer, :float, :time, :date, :boolean
+        ActiveRecord::Base.connection.lookup_cast_type(type).type_cast_from_user(value)
       else value
         # Nothing.
       end
